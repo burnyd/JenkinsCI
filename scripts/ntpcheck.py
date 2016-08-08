9235050c9b77367f03b544aa6fe680af9ba1897a
burnyd <burnyd@gmail.com>
2016-08-08 16:18:58 -0400

    second push@

---
+++ scripts/ntpcheck.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+#Simple test to check to see if the ntp server is working properly.
+
+import pyeapi
+import json
+import time
+import jinja2
+
+veos02 = pyeapi.connect_to('veos02')
+ntpstatus = veos02.enable('show ntp status')
+ntpstatusvalues = ntpstatus[0]['result'].values()
+
+for i in ntpstatusvalues:
+    if i.find("unsynchronised") != -1:
+        print "It didnt work"	
